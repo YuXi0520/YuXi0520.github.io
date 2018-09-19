6a5442c8afaee64dc65f96c34fc3e2b78c27382e
YuXi0520 <41014381+YuXi0520@users.noreply.github.com>
2018-09-19 15:13:47 +0800

    Add files via upload

--- web-mobile/src/settings.js
+++ web-mobile/src/settings.js
@@ -1,1 +1,216 @@
-window._CCSettings={platform:"web-mobile",groupList:["player","enemy","equip"],collisionMatrix:[[false,true],[true,false,true],[false,true,false]],rawAssets:{assets:{"6d8skRtjJCp4fPoRbTunV3":["Animation/player/打1.png",0],d8APfwLg5FZYu1tlfKc0ic:["Animation/player/打2.png",0],c5QDiT51dDbpk9d34OZ9pe:["Animation/player/打3.png",0],c4GMAlmG9CrKBCB2eH9HaC:["Animation/player/打4.png",0],a0ekcI0I5G76AZvtZ4p4I8:["Texture/JoystickBG.png",0],"92T659jjRFrYFhogIT4rhw":["Texture/JoystickBar.png",0],"0e+/iqM8VJEJqc219jRiWz":["Texture/ball3.png",0],c3feeK2RNAeaX0GQxD0oPO:["Texture/enemy0.png",0],"2b53dqJ/JNXp/jvl1mraz6":["Texture/enemy1.png",0],"e2Xd+U0HhHrJ2JiApgHxSy":["Texture/score_frame.png",0],"361WdmTXRCO7Ndsr9fEA4j":["Texture/start_btn.png",0],"83n/J4sAFB4Il5my7hLBDz":["Texture/乾坤圈.png",0],"80s6lKvcNJi7C9PAPH5SGV":["Texture/待机哪吒_1 .png",0],e4VH5eprZFmbTzLYlUq0Xw:["Texture/最终版开始界面-da.png",0],"e1Uf29WJZEIpTzEX0mJA+P":["Texture/最终版开始界面-long.png",0],"9e7qZ1A/dCE5Ddv509O//X":["Texture/最终版开始界面-ren.png",0],"4b+aRTVRRC1rG1k/WuxEen":["Texture/最终版开始界面-zi.png",0],"2cSa/xmhJJsbSbEzmSKrUg":["Texture/结束界面-再玩一次.png",0],"053GEe75NCEJZZV2vc0PKh":["Texture/结束界面1.png",0],"24RWwNnuFEV5FsCNJ+88dw":["Texture/背景_海 (1).png",0],e8C3atKl1EWrFnowQ4qagg:["Texture/背景_海.png",0],"432TTw0DpK5pp90NxlUPjT":["juqing-tu/153283191281613.png",0],"8a87Gt2p1JyKXOaukWAnK4":["juqing-tu/153283211887971.png",0],"6fdMYjC+lC5bnrwEZGLLKm":["juqing-tu/图片2.1.png",0],"beRelflfZOZ7q5/8F92Ygm":["juqing-tu/图片2.2.png",0],"44Wnc7dWlJyKbWJbEW094f":["juqing-tu/图片2.3.png",0],"d8Y3K3AiFN/K1DBwO6Ky+7":["juqing-tu/海浪2.png",0],acP1RNxvlBWr1CgB5UxZkV:["juqing-tu/结束界面1.png",0]},internal:{"6eBWFz0oVHPLIGQKf/9Thu":["image/default_sprite.png",0]}},assetTypes:["cc.Texture2D"],launchScene:"db://assets/Scene/start-scene.fire",scenes:[{url:"db://assets/Scene/start-scene.fire",uuid:2},{url:"db://assets/Scene/juqing-scene.fire",uuid:1},{url:"db://assets/Scene/main-scene.fire",uuid:5},{url:"db://assets/Scene/game-over-scene.fire",uuid:0},{url:"db://assets/Scene/win-scene.fire",uuid:"dezyxn/9pIx5GvJovFuTwb"}],packedAssets:{"014ff8bef":[0,"384Y9lRfdF7KMGlAAwZxHs","5abFX7i4NIMbs4zpF/BMo4","7dJ9/Q1QlHvKNdfa9JgHN9","a7tGS5v59Mi5cU9mxDxol9"],"08873416e":["148ENPjH5FNK3A8XYaLqIr","1ahmEfeXhBLqL8roF2bBYQ",4,1,"7cXslLSeFKYavOtH4yM1aQ","b7fMDxSz1FDpkWVmv7UNr9","d376Jk2OBO+rJJAyYUMmcL","d5Ja4lXExOt4WB5LZdTEpL",3,"f06MuP6ghGmqYcrfpQBHsK","f1fYrDdKdA1qB0iPC29aAq"],"09e8b509a":["2d+121ECtDgbTesdzBJB8S","6dedZ84WFC7rJVrBfgsoc2","70Nk9tO45D6J1BoNbc0cZm",2,"9aFS5jEStKnqyzZs2qvKoL",3],"0b8acbef5":["09nf80NTZHCbt6EvIvUHI5","1bSNWVcQpALImgiUiGjtz/",4,"30P/s90XNCdrsBXEv2jhac","3aTRRE/D1DiLrEUSNS4qD6","4bey8MfrFCgIdNObBnVoEe","4dDfiwgNtFBYGmbKUF2TzG","6aUBPKorRF965r92wf89bs","78LG1EI29AppnOT+/gwd3a","83Hdnjl39PjbJdugl4BJ3S","84xDwjQAtELZZwXDtxj9Pc","8c20Sso/ZEn7NUfNSM+EBh",5,"aepl5fCLhJ+7ExuUMg/36S","b3+lzg7LVEcrlMtUR7k/0F","c5ByvhlNBKkLiksBXUx/gU","e8HhAiYRVG9K85VXv90+Uf","f8MbbHWwJA35oc6egdP+9p"]},orientation:"landscape",uuids:["0fYdhVJexBA5ElWAQIJxjK","340YsnsL5OmZRIkTiixEQB","85wmr3iKlA4Yy2evSjkNJS","e3nSax22hKprQqYSAA2Ri/","26dbgPxQJM1YNrMo/O+0wM","95UrTgtRlEUrsEmHbe7DlN"]};
+window._CCSettings = {
+    platform: "web-mobile",
+    groupList: [
+        "player",
+        "enemy",
+        "equip"
+    ],
+    collisionMatrix: [
+        [
+            false,
+            true
+        ],
+        [
+            true,
+            false,
+            true
+        ],
+        [
+            false,
+            true,
+            false
+        ]
+    ],
+    rawAssets: {
+        assets: {
+            "6d8skRtjJCp4fPoRbTunV3": [
+                "Animation/player/打1.png",
+                "cc.Texture2D"
+            ],
+            d8APfwLg5FZYu1tlfKc0ic: [
+                "Animation/player/打2.png",
+                "cc.Texture2D"
+            ],
+            c5QDiT51dDbpk9d34OZ9pe: [
+                "Animation/player/打3.png",
+                "cc.Texture2D"
+            ],
+            c4GMAlmG9CrKBCB2eH9HaC: [
+                "Animation/player/打4.png",
+                "cc.Texture2D"
+            ],
+            a0ekcI0I5G76AZvtZ4p4I8: [
+                "Texture/JoystickBG.png",
+                "cc.Texture2D"
+            ],
+            "92T659jjRFrYFhogIT4rhw": [
+                "Texture/JoystickBar.png",
+                "cc.Texture2D"
+            ],
+            "0e+/iqM8VJEJqc219jRiWz": [
+                "Texture/ball3.png",
+                "cc.Texture2D"
+            ],
+            c3feeK2RNAeaX0GQxD0oPO: [
+                "Texture/enemy0.png",
+                "cc.Texture2D"
+            ],
+            "2b53dqJ/JNXp/jvl1mraz6": [
+                "Texture/enemy1.png",
+                "cc.Texture2D"
+            ],
+            "e2Xd+U0HhHrJ2JiApgHxSy": [
+                "Texture/score_frame.png",
+                "cc.Texture2D"
+            ],
+            "361WdmTXRCO7Ndsr9fEA4j": [
+                "Texture/start_btn.png",
+                "cc.Texture2D"
+            ],
+            "83n/J4sAFB4Il5my7hLBDz": [
+                "Texture/乾坤圈.png",
+                "cc.Texture2D"
+            ],
+            "80s6lKvcNJi7C9PAPH5SGV": [
+                "Texture/待机哪吒_1 .png",
+                "cc.Texture2D"
+            ],
+            "e1Uf29WJZEIpTzEX0mJA+P": [
+                "Texture/最终版开始界面-long.png",
+                "cc.Texture2D"
+            ],
+            "9e7qZ1A/dCE5Ddv509O//X": [
+                "Texture/最终版开始界面-ren.png",
+                "cc.Texture2D"
+            ],
+            "4b+aRTVRRC1rG1k/WuxEen": [
+                "Texture/最终版开始界面-zi.png",
+                "cc.Texture2D"
+            ],
+            "2cSa/xmhJJsbSbEzmSKrUg": [
+                "Texture/结束界面-再玩一次.png",
+                "cc.Texture2D"
+            ],
+            "053GEe75NCEJZZV2vc0PKh": [
+                "Texture/结束界面1.png",
+                "cc.Texture2D"
+            ],
+            "24RWwNnuFEV5FsCNJ+88dw": [
+                "Texture/背景_海 (1).png",
+                "cc.Texture2D"
+            ],
+            e8C3atKl1EWrFnowQ4qagg: [
+                "Texture/背景_海.png",
+                "cc.Texture2D"
+            ],
+            "432TTw0DpK5pp90NxlUPjT": [
+                "juqing-tu/153283191281613.png",
+                "cc.Texture2D"
+            ],
+            "8a87Gt2p1JyKXOaukWAnK4": [
+                "juqing-tu/153283211887971.png",
+                "cc.Texture2D"
+            ],
+            "6fdMYjC+lC5bnrwEZGLLKm": [
+                "juqing-tu/图片2.1.png",
+                "cc.Texture2D"
+            ],
+            "beRelflfZOZ7q5/8F92Ygm": [
+                "juqing-tu/图片2.2.png",
+                "cc.Texture2D"
+            ],
+            "44Wnc7dWlJyKbWJbEW094f": [
+                "juqing-tu/图片2.3.png",
+                "cc.Texture2D"
+            ],
+            "d8Y3K3AiFN/K1DBwO6Ky+7": [
+                "juqing-tu/海浪2.png",
+                "cc.Texture2D"
+            ],
+            acP1RNxvlBWr1CgB5UxZkV: [
+                "juqing-tu/结束界面1.png",
+                "cc.Texture2D"
+            ]
+        },
+        internal: {
+            "6eBWFz0oVHPLIGQKf/9Thu": [
+                "image/default_sprite.png",
+                "cc.Texture2D"
+            ]
+        }
+    },
+    launchScene: "db://assets/Scene/start-scene.fire",
+    scenes: [
+        {
+            url: "db://assets/Scene/start-scene.fire",
+            uuid: "85wmr3iKlA4Yy2evSjkNJS"
+        },
+        {
+            url: "db://assets/Scene/juqing-scene.fire",
+            uuid: "340YsnsL5OmZRIkTiixEQB"
+        },
+        {
+            url: "db://assets/Scene/main-scene.fire",
+            uuid: "95UrTgtRlEUrsEmHbe7DlN"
+        },
+        {
+            url: "db://assets/Scene/game-over-scene.fire",
+            uuid: "0fYdhVJexBA5ElWAQIJxjK"
+        },
+        {
+            url: "db://assets/Scene/win-scene.fire",
+            uuid: "dezyxn/9pIx5GvJovFuTwb"
+        }
+    ],
+    packedAssets: {
+        "014ff8bef": [
+            "0fYdhVJexBA5ElWAQIJxjK",
+            "384Y9lRfdF7KMGlAAwZxHs",
+            "5abFX7i4NIMbs4zpF/BMo4",
+            "7dJ9/Q1QlHvKNdfa9JgHN9",
+            "a7tGS5v59Mi5cU9mxDxol9"
+        ],
+        "08873416e": [
+            "148ENPjH5FNK3A8XYaLqIr",
+            "1ahmEfeXhBLqL8roF2bBYQ",
+            "26dbgPxQJM1YNrMo/O+0wM",
+            "340YsnsL5OmZRIkTiixEQB",
+            "7cXslLSeFKYavOtH4yM1aQ",
+            "b7fMDxSz1FDpkWVmv7UNr9",
+            "d376Jk2OBO+rJJAyYUMmcL",
+            "d5Ja4lXExOt4WB5LZdTEpL",
+            "e3nSax22hKprQqYSAA2Ri/",
+            "f06MuP6ghGmqYcrfpQBHsK",
+            "f1fYrDdKdA1qB0iPC29aAq"
+        ],
+        "0b8acbef5": [
+            "09nf80NTZHCbt6EvIvUHI5",
+            "1bSNWVcQpALImgiUiGjtz/",
+            "26dbgPxQJM1YNrMo/O+0wM",
+            "30P/s90XNCdrsBXEv2jhac",
+            "3aTRRE/D1DiLrEUSNS4qD6",
+            "4bey8MfrFCgIdNObBnVoEe",
+            "4dDfiwgNtFBYGmbKUF2TzG",
+            "6aUBPKorRF965r92wf89bs",
+            "78LG1EI29AppnOT+/gwd3a",
+            "83Hdnjl39PjbJdugl4BJ3S",
+            "84xDwjQAtELZZwXDtxj9Pc",
+            "8c20Sso/ZEn7NUfNSM+EBh",
+            "95UrTgtRlEUrsEmHbe7DlN",
+            "aepl5fCLhJ+7ExuUMg/36S",
+            "b3+lzg7LVEcrlMtUR7k/0F",
+            "c5ByvhlNBKkLiksBXUx/gU",
+            "e8HhAiYRVG9K85VXv90+Uf",
+            "f8MbbHWwJA35oc6egdP+9p"
+        ],
+        "0eb6de7f6": [
+            "2d+121ECtDgbTesdzBJB8S",
+            "6dedZ84WFC7rJVrBfgsoc2",
+            "70Nk9tO45D6J1BoNbc0cZm",
+            "85wmr3iKlA4Yy2evSjkNJS",
+            "e3nSax22hKprQqYSAA2Ri/"
+        ]
+    },
+    orientation: "landscape",
+    debug: true
+};
